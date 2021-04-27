b0b24cb5c972136bf3cfdfbae110938d4bf8621e
João Victor <79094205+Jaunz@users.noreply.github.com>
2021-04-27 10:02:09 -0300

    Projeto finalizado
    Portfólio

---
+++ PortifolioPSEL/Js/Js.js
@@ -0,0 +1,17 @@
+function mostrar(click){
+
+    const caixas = document.querySelectorAll('.conhecimentos-conteudos');
+    for (const caixa of caixas) {
+        caixa.classList.add('oculto');
+    }
+    const caixa = document.querySelector('.conhecimentos-conteudos'+click);
+    caixa.classList.remove('oculto');
+
+    const botoes = document.querySelectorAll('.retangle button');
+    for (const botao of botoes) {
+        botao.classList.remove('ativo');
+    }
+    const botao = document.querySelector('button'+click);
+    botao.classList.add('ativo');
+
+}
